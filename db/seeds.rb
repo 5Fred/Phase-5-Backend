0ed07b94a893369fdeb5991f1714e4e49c2ac653
emmanuel gichigo mukira <emmanuelgichigo@gmail.com>
2023-07-27 07:52:59 +0300

    Seeded database with image_urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,27 +27,3 @@ end
 puts "Done seeding"
 
 
-# # seeds.rb
-# puts "Started seeding"
-# require 'dotenv/load'
-# require 'news-api'
-
-# api_key = ENV['API_KEY']
-# newsapi = News.new(api_key)
-
-# # Get top headlines from the News API
-# response = newsapi.get_top_headlines(sources: 'bbc-news', language: 'en', pageSize: 20)
-# articles = response
-
-# # Seed news articles
-# articles.each do |article|
-#   NewsArticle.create!(
-#     headline: article.title,
-#     summary: article.description,
-#     content: article.content,
-#     publication_date: article.publishedAt,
-#     sentiment: 'neutral' # Assuming 'neutral' sentiment for now
-#   )
-# end
-
-# puts "Done seeding"
