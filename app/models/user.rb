991f7aeb21bad7406e8b9ff9f42ea2624b3b49cd
emmanuel gichigo mukira <emmanuelgichigo@gmail.com>
2023-07-30 21:06:52 +0300

    Almost done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+      has_many :bookmarks
+      has_many :user_preferences
+    
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true
     validates :password, presence: true, length: { minimum: 6 }
