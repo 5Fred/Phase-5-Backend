356a97e5b7b66c46724271080ce9e8564330dc78
emmanuel gichigo mukira <emmanuelgichigo@gmail.com>
2023-08-03 13:07:09 +0300

    Bookmark working

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   # Authentication route
   post '/auth/login', to: 'authentication#login'
+  resources :bookmarks, only: [:index, :create, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
