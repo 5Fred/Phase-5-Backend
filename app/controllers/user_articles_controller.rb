578ffed25d36fd3d278c3272e8a23cecce7348e9
emmanuel gichigo mukira <emmanuelgichigo@gmail.com>
2023-08-06 11:37:40 +0300

    Core deliverables working

--- app/controllers/user_articles_controller.rb
+++ app/controllers/user_articles_controller.rb
@@ -2,7 +2,7 @@ class UserArticlesController < ApplicationController
     before_action :authenticate_user
 
   def index
-    user_articles = current_user.news_articles
+    user_articles = @current_user.news_articles
     render json: user_articles
   end
 end
